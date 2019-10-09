b39b8cb7b3c8332f8fe253f9b3678b1d8fb87049
Vorinelf <vor-on-inlf@mail.ru>
2019-10-09 18:53:52 +0300

    First Commit

--- src/main/java/com/voronov/springDemo/controller/MainController.java
+++ src/main/java/com/voronov/springDemo/controller/MainController.java
@@ -3,27 +3,22 @@ package com.voronov.springDemo.controller;
 import com.voronov.springDemo.domain.Message;
 import com.voronov.springDemo.domain.User;
 import com.voronov.springDemo.repository.MessageRepo;
-import net.bytebuddy.implementation.bind.MethodDelegationBinder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.naming.Binding;
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 @Controller
 public class MainController {
